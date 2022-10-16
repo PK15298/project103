0e85a797a704d777cf0acc90546ce57cc1d83eb4
PK15298 <85899531+PK15298@users.noreply.github.com>
2022-10-16 16:39:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-
+camera = document.getElementById("camera");
 Webcam.set({
     width:350,
     height:300,
-    image_format : 'jpeg',
-    jpeg_quality:90
+    image_format : 'png',
+    png_quality:90
   });
-
-  camera = document.getElementById("camera");
-
 Webcam.attach( '#camera' );
 
       
@@ -18,8 +15,7 @@ function take_snapshot()
     });
 }
 
-  console.log('ml5 version:', ml5.version);
-  
+console.log('ml5 version:', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
 
   function modelLoaded() {
